bd7cd0a1eab17acc900c050057608d7c4a2f48c0
jon4hz <me@jon4hz.io>
2025-12-19 13:38:44 +0100

    chore: adjust comment

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 var (
-	// Version is the current version of gmotd
+	// Version is the current version of awoolt.
 	Version = "devel"
 	// Commit is the git commit hash of the current version.
 	Commit = "none"
